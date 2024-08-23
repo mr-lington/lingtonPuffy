567ce01a1cb5550953c92d787a4cf744fe14b3b8
Fortune Green <green@MacBook-Pro.local>
2024-08-23 22:03:28 +0100

    fixed UI and frontend

--- src/main/java/com/androidgreen/demoapp/DemoAppApplication.java
+++ src/main/java/com/androidgreen/demoapp/DemoAppApplication.java
@@ -2,12 +2,18 @@ package com.androidgreen.demoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class DemoAppApplication {
-
+public class DemoAppApplication extends SpringBootServletInitializer {
   public static void main(String[] args) {
     SpringApplication.run(DemoAppApplication.class, args);
   }
-
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(DemoAppApplication.class);
+  }
 }
--- src/main/java/com/androidgreen/demoapp/LintonController.java
+++ src/main/java/com/androidgreen/demoapp/LintonController.java
@@ -1,15 +1,16 @@
 package com.androidgreen.demoapp;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class LintonController {
 
-  @GetMapping
+  @GetMapping("/")
   public String index(){
-    return "Linton App deployed and ready";
+    return "index";
   }
 }
